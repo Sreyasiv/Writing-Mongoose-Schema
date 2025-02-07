ae29ff1e9ee694e9423a2016adeb4d94faa5e5e3
Sreya Sivakumar <sreyas@Sreyas-MacBook-Air.local>
2025-02-07 09:49:34 +0530

    Updated_with_mon

---
+++ schema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  roles: {
+    type: [String],
+    default: ["user"]
+  },
+  profile: {
+    firstName: String,
+    lastName: String,
+    age: Number
+  },
+  lastLogin: {
+    type: Date,
+    default: null
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
